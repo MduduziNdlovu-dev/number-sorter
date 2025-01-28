6a520140afebde50b26c8216a4817610d5371a28
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-01-28 15:08:43 +0200

    feat: Implement insertion sort algorithm and integrate it with the UI for sorting input values

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-    const sortedValues = bubbleSort(inputValues);
+    const sortedValues = selectionSort(inputValues);
     updateUI(inputValues);
 
 }
@@ -23,7 +23,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
-            console.log(array , array[j], array[j+1])
             if (array[j]> array[j+1]) {
                 const temp = array[j];
                 array[j] = array[j+1];
@@ -33,6 +32,22 @@ const bubbleSort = (array) => {
     }
     return array
 }
+
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
+  
+      for (let j = i + 1; j < array.length; j++) {
+        if(array[j] < array[minIndex]){
+          minIndex = j;
+        }
+      }
+      const temp = array[i];
+      array[i] = array[minIndex];  
+      array[minIndex] = temp;
+    }
+    return array;
+  }
 
 sortButton.addEventListener("click", sortInputArray)
 
