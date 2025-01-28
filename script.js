57588255278115d3d001e33ef6d989d1b1673542
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-01-28 14:34:57 +0200

    Feat: Add sorting functionality and dynamic UI updates for input array

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
+const sortButton = document.getElementById("sort");
+
+
+const sortInputArray = (event) => {
+    event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+    updateUI(inputValues)
+
+}
+
+const updateUI = (array = []) => {
+    array.forEach((num, i )=> {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+
+    });
+
+}
+
+sortButton.addEventListener("click", sortInputArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Load saved theme from localStorage
 const savedTheme = localStorage.getItem("theme");
